a242fe4405d9ce55a78eff1683591990af3f80dc
Youssef Abou Eljihad <yousef.aboueljihad@gmail.com>
2026-04-11 16:23:35 +0100

    Time: 0 ms (100.00%) | Memory: 44.8 MB (35.68%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0 ; 
+        int end = nums.length - 1 ; 
+        int mid = start + (end - start) / 2;
+        while(start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid  ; 
+            else if (target > nums[mid]) start = mid + 1; 
+            else end = mid - 1;
+            mid = (start + end)/2; 
+        }
+        if (target < nums[0]) return mid ; 
+        else return mid + 1 ; 
+    }
+}

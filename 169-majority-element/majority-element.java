d824cee19006b9c32d45f6325329e7195ab5fa2e
Youssef Abou Eljihad <yousef.aboueljihad@gmail.com>
2026-07-16 22:07:41 +0100

    Time: 2 ms (70.20%) | Memory: 55.8 MB (18.09%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0 ; 
+        int candidate = 0 ;
+        for (int n : nums) {
+            if (count == 0) candidate = n ; 
+            count += (n == candidate) ? 1 : -1 ; 
+        } 
+        return candidate ; 
+    }
+}
